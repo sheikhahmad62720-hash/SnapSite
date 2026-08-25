f7e89eac7828afd9de2ca123a800fef298ccb79c
Muhammad Ahmad <sheikhahmad62720@gmail.com>
2026-08-25 20:55:53 +0500

    feat: add single-page website screenshot generator

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\ScreenshotController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return inertia('Welcome');
 });
+
+Route::post('/screenshots/generate', [ScreenshotController::class, 'generate'])
+    ->name('screenshot.generate');
+
+Route::get('/screenshots/download/{filename}', [ScreenshotController::class, 'download'])
+    ->name('screenshot.download');
